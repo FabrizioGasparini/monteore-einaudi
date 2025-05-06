ec2aa48a92d18ac122822ba562d17c91ddec22c3
FabrizioGasparini <makerfaffa@gmail.com>
2025-05-06 17:36:49 +0200

    Update manage.tsx

--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -374,7 +374,7 @@ export default function ManageActivities() {
             </div>
             {
                 isEditing ?
-                <div className='fixed h-fit w-9/12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#252525] rounded-lg bg-opacity-80 text-center p-4 border-gray-400 border-2 shadow-lg max-h-[90%] overflow-y-scroll'>
+                <div className='fixed h-auto w-9/12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#252525] rounded-lg bg-opacity-80 text-center p-4 border-gray-400 border-2 shadow-lg max-h-[90%] overflow-y-scroll'>
                     <div className="topbar">
                         <h2 className='text-2xl font-bold'>{isCreating ? "Crea" : "Modifica"} Attività</h2>
                         <button onClick={() => setIsEditing(false)} className='absolute top-4 right-4 text-red-400 hover:text-red-600 transition-colors duration-200 ease-in-out'>✖</button>        
@@ -419,7 +419,7 @@ export default function ManageActivities() {
             }
             {
                 isWatching ?
-                <div className='fixed h-fit w-9/12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#252525] rounded-lg bg-opacity-80 text-center p-4 border-gray-400 border-2 shadow-lg max-h-[90%] overflow-y-scroll'>
+                <div className='fixed h-auto w-9/12 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#252525] rounded-lg bg-opacity-80 text-center p-4 border-gray-400 border-2 shadow-lg max-h-[90%] overflow-y-scroll'>
                     <div className="topbar mb-2">
                         <h2 className='text-2xl font-bold'>Partecipanti Attività</h2>
                         <button onClick={() => setIsWatching(false)} className='absolute top-4 right-4 text-red-400 hover:text-red-600 transition-colors duration-200 ease-in-out'>✖</button>        
@@ -430,7 +430,7 @@ export default function ManageActivities() {
                             [...Array((maxDurataEdit) / durataEdit)].map((x, i) => <option value={i.toString()} key={i}>{formatTime(new Date(0, 0, 0, new Date(activities.find((activity: Activity) => activity.id === activityId)?.startTime).getHours() + durataEdit * (i), 0))} - {formatTime(new Date(0, 0, 0, new Date(activities.find((activity: Activity) => activity.id === activityId)?.startTime).getHours() + durataEdit * (i + 1), 0))}</option>)
                         }
                     </select>
-                    <ul role="list" className="flex flex-wrap gap-4 mt-4 items-center justify-center w-full overflow-y-scroll max-h-[90%] h-fit" >
+                    <ul role="list" className="flex flex-wrap gap-4 mt-4 items-center justify-center w-full overflow-y-scroll max-h-[90%] h-auto" >
                             {
                             activities.find((activity: Activity) => activity.id == activityId).subscriptions?.length == 0 ? <p>Nessuna iscrizione trovata</p> :
                             activities.find((activity: Activity) => activity.id == activityId).subscriptions?.map((subscription: Subscription) => {
